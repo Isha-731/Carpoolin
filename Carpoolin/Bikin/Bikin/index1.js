10e92a4423df462f7b6404a0e164d70e6b86eed7
Isha <isha16112004@gmail.com>
2024-08-14 15:04:03 +0530

    first commit

---
+++ Carpoolin/Bikin/Bikin/index1.js
@@ -0,0 +1,7 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('animContainer'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'C:\Users\Dell\Downloads\Bikin\Animation - 1709275343781.json' // lottie file path
+  })
